f3f012cfceb8e2f3911871bcf70c59f50e973a46
c3llkn1ght <164270614+c3llkn1ght@users.noreply.github.com>
2026-02-20 19:15:41 -0500

    check for root and randomize ips
    will better obfuscate user :3

--- console.py
+++ console.py
@@ -11,155 +11,191 @@ import os
 import shlex
 import subprocess
 import sys
+import random
+import ipaddress
 
 logging.basicConfig(
-	level=logging.INFO,
-	format='\033[1;36m[TORmap]\033[0m %(message)s',
+    level=logging.INFO,
+    format='\033[1;36m[TORmap]\033[0m %(message)s',
 )
 logger = logging.getLogger('TORmap')
 
 # Checks for root perms
 def checkRoot():
-	if os.geteuid() != 0:
-		print("Script must be run as root!")
-		sys.exit(1)
+    if os.geteuid() != 0:
+        print("Script must be run as root!")
+        sys.exit(1)
+
+def gen_decoys(count=4) -> str:
+    """
+    gen a decoy string for -D
+    could use something more robust than defined ranges but whatever
+    """
+
+    ranges = [
+        "138.197.0.0/16",
+        "185.228.168.0/24",
+        "23.67.0.0/16",
+    ]
+
+    def rand_cidr_ips(cidr: str) -> str:
+        net = ipaddress.IPv4Network(cidr, strict=False)
+        network_int = int(net.network_address)
+        broadcast_int = int(net.broadcast_address)
+        random_int = random.randint(network_int + 1, broadcast_int - 1)
+        return str(ipaddress.IPv4Address(random_int))
+
+    decoys = set()
+
+    while len(decoys) < count:
+        cidr = random.choice(ranges)
+        decoys.add(rand_cidr_ips(cidr))
+
+    decoy_list = list(decoys)
+    insert_pos = random.randint(0, len(decoy_list))
+    decoy_list.insert(insert_pos, "ME")
+
+    return ",".join(decoy_list)
+
 
 # Class that contains the console
 class TORmapConsole(cmd.Cmd):
-	prompt = "TORmap > "
+    prompt = "TORmap > "
 
-	# variables
-	def __init__(self):
-		super().__init__()
-		self.target = ""
-		self.flags = []
-		self.allowed_flags = {
-			"-sS", "-sV", "-p-", "-Pn", "-O", "-n", "-f"
-		}
-		self.valuesFlag = {
-			"-p", "--spoof-mac", "--data-length", "--max-retries"
-		}
+    # variables
+    def __init__(self):
+        super().__init__()
+        self.target = ""
+        self.flags = []
+        self.allowed_flags = {
+            "-sS", "-sV", "-p-", "-Pn", "-O", "-n", "-f"
+        }
+        self.valuesFlag = {
+            "-p", "--spoof-mac", "--data-length", "--max-retries"
+        }
 
-	# on input "set target" or "set flags"
-	def do_set(self, args):
-		parts = shlex.split(args)
-		if len(parts) < 2:
-			logger.warning("Usage: set [target|flags] <value>")
-			return
+    # on input "set target" or "set flags"
+    def do_set(self, args):
+        parts = shlex.split(args)
+        if len(parts) < 2:
+            logger.warning("Usage: set [target|flags] <value>")
+            return
 
-		key, value = parts[0], " ".join(parts[1:])
-		if key == "target":
-			self.target = value
-			logger.info(f"Target set to: {self.target}")
+        key, value = parts[0], " ".join(parts[1:])
+        if key == "target":
+            self.target = value
+            logger.info(f"Target set to: {self.target}")
 
-		elif key == "flags":
-			rawFlags = shlex.split(value)
-			validFlags = []
-			i = 0
-			while i < len(rawFlags):
-				flag = rawFlags[i]
+        elif key == "flags":
+            rawFlags = shlex.split(value)
+            validFlags = []
+            i = 0
+            while i < len(rawFlags):
+                flag = rawFlags[i]
 
-				if flag in self.valuesFlag:
-					if i + 1 < len(rawFlags):
-						param = rawFlags[i + 1]
+                if flag in self.valuesFlag:
+                    if i + 1 < len(rawFlags):
+                        param = rawFlags[i + 1]
 
-						if flag == "-p-" or param == "-":
-							validFlags.append("-p-")
-						else:
-							validFlags.extend([flag, param])
-						i += 2
-						continue
-					else:
-						logger.warning(f"Missing value for {flag}")
-						break
+                        if flag == "-p-" or param == "-":
+                            validFlags.append("-p-")
+                        else:
+                            validFlags.extend([flag, param])
+                        i += 2
+                        continue
+                    else:
+                        logger.warning(f"Missing value for {flag}")
+                        break
 
-				elif flag in self.allowed_flags:
-					validFlags.append(flag)
-					i += 1
-					continue
+                elif flag in self.allowed_flags:
+                    validFlags.append(flag)
+                    i += 1
+                    continue
 
-				else:
-					logger.warning(f"Ignoring unrecognized flag: {flag}")
-					i += 1
+                else:
+                    logger.warning(f"Ignoring unrecognized flag: {flag}")
+                    i += 1
 
-			self.flags = validFlags
-			logger.info(f"Flags set to: {' '.join(self.flags)}")
-		else:
-			logger.warning("Unknown key. Use 'target' or 'flags'")
+            self.flags = validFlags
+            logger.info(f"Flags set to: {' '.join(self.flags)}")
+        else:
+            logger.warning("Unknown key. Use 'target' or 'flags'")
 
-	# on input "show"
-	def do_show(self, args):
-		logger.info("Current settings:")
-		logger.info(f"Target: {self.target}")
-		logger.info(f"Flags: {' '.join(self.flags)}")
+    # on input "show"
+    def do_show(self, args):
+        logger.info("Current settings:")
+        logger.info(f"Target: {self.target}")
+        logger.info(f"Flags: {' '.join(self.flags)}")
 
-	# on input "run"
-	def do_run(self, args):
-		if not self.target:
-			logger.error("Target not set. Use: set target <IP/host>")
-			return
-		command = ["proxychains", "nmap"] + self.flags + ["-D 185.228.168.9,23.67.253.113,8.26.56.26,ME,138.197.0.0", "-vvv"] + [self.target]
-		logger.info(f"Running: {' '.join(command)}")
-		try:
-			subprocess.run(command)
-		except Exception as e:
-			logger.error(f"Execution failed: {e}")
+    # on input "run"
+    def do_run(self, args):
+        if not self.target:
+            logger.error("Target not set. Use: set target <IP/host>")
+            return
+        decoys = gen_decoys(count=5)
+        command = ["proxychains", "nmap"] + self.flags + ["-D", decoys, "-vvv"] + [self.target]
+        logger.info(f"Running: {shlex.join(command)}")
+        try:
+            subprocess.run(command)
+        except Exception as e:
+            logger.error(f"Execution failed: {e}")
 
-	# on input "exit"
-	def do_exit(self, args):
-		logger.info("Exiting...")
-		return True
+    # on input "exit"
+    def do_exit(self, args):
+        logger.info("Exiting...")
+        return True
 
-	# on input "help" or "?"
-	def do_help(self, args):
-		helpTxt = """
+    # on input "help" or "?"
+    def do_help(self, args):
+        helpTxt = """
 [console help]
 Available Commands:
 
-	set target <ip>		Set the target IP or Host
-	set flags <options>	Set the Nmap flags
-	show			Show the current settings
-	run			Run Nmap through proxychains
-	exit			Exit the console
-	help			Show this message
+    set target <ip>		Set the target IP or Host
+    set flags <options>	Set the Nmap flags
+    show			Show the current settings
+    run			Run Nmap through proxychains
+    exit			Exit the console
+    help			Show this message
 
 
 Flags Available (Use with: set flags <flags>):
 
-	Scan Tags:
-	  -sS			TCP/SYN (Stealth) scan
-	  -sV			Service Version Detection
-	  -p			Specified Ports to scan
+    Scan Tags:
+      -sS			TCP/SYN (Stealth) scan
+      -sV			Service Version Detection
+      -p			Specified Ports to scan
 
-	Host Discovery:
-	  -Pn			Treat hosts as online (skip ping)
+    Host Discovery:
+      -Pn			Treat hosts as online (skip ping)
 
-	Agressiveness:
-	  -O			Enable OS detection
-	  -n			skip DNS resolution
-	  --max-retries <n>	Number of times nmap will retry sending a probe
+    Agressiveness:
+      -O			Enable OS detection
+      -n			skip DNS resolution
+      --max-retries <n>	Number of times nmap will retry sending a probe
 
-	Bypass:
-	  -f			Fragment Packets
-	  --spoof-mac		Spoofs mac address (Cisco, Apple, Dell, etc.)
-	  --data-length <n>	Append random data to packets
+    Bypass:
+      -f			Fragment Packets
+      --spoof-mac		Spoofs mac address (Cisco, Apple, Dell, etc.)
+      --data-length <n>	Append random data to packets
 
-	Note: you can combine flags by typing 'set flags -sS -Pn -n'
+    Note: you can combine flags by typing 'set flags -sS -Pn -n'
 
 """
-		logger.info(helpTxt.strip())
+        logger.info(helpTxt.strip())
 
 
 # Confirms Launch of terminal
 def launch():
-	logger.info("Welcome to the TORmap console!")
-	logger.info("Map a network like Anon")
-	shell = TORmapConsole()
-	shell.cmdloop()
+    logger.info("Welcome to the TORmap console!")
+    logger.info("Map a network like Anon")
+    checkRoot()
+    shell = TORmapConsole()
+    shell.cmdloop()
 
 if __name__ == "__main__":
-	if len(sys.argv) > 1 and sys.argv[1] == "launch":
-		launch()
-	else:
-		logger.info("Usage: ./console.py launch")
+    if len(sys.argv) > 1 and sys.argv[1] == "launch":
+        launch()
+    else:
+        logger.info("Usage: ./console.py launch")
 
